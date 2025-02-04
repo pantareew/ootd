97a9fe0d597938b17eabae7cdadfcdfed9c17fc9
pantareew <pantaree1459@gmail.com>
2025-02-04 18:50:32 +1100

    add start script for Heroku

--- vue.config.js
+++ vue.config.js
@@ -2,4 +2,7 @@ const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: "/cos30043/s103837447/ootd",
+  devServer: {
+    port: process.env.PORT || 8080,
+  },
 });
