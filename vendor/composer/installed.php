60123e7e6341f63b6f62b9fa6e82fad8e517d657
pantareew <pantaree1459@gmail.com>
2025-02-04 19:05:16 +1100

    Set up Heroku deployment

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'pantareew/ootd',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '97a9fe0d597938b17eabae7cdadfcdfed9c17fc9',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'pantareew/ootd' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '97a9fe0d597938b17eabae7cdadfcdfed9c17fc9',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
